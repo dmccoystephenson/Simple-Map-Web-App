e43a947673eed6b469e4aaeb9098f766747d1b15
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-09-08 13:30:32 -0600

    Made some tweaks to the project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Experimenting with ArcGIS and React</h2>
+        <h2>Simple Map Web App</h2>
         <p>(map goes here)</p>
       </header>
     </div>
